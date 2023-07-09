b584bf83a1b3bfea2186532b87e4683c2fd33225
unknown <ricardoalexander1904gmail.com>
2023-07-08 20:56:28 -0500

    Primera funcion para el programa

--- Proyecto Programacion.c
+++ Proyecto Programacion.c
@@ -8,4 +8,25 @@ typedef struct
     char Ingredientes[100];
     float Precio;
 } Plato;
+// Funciones para guardar los platos y los ingredientes en archivos
+void Guardar_Platos(Plato Platos[], int Numero_Platos)
+{
+    FILE *archivo;
+    archivo = fopen("Platos.txt", "w");
+    // si el archivo no se abre muestra un mensaje de error y retorna
+    if (archivo == NULL)
+    {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
 
+    // Guardar los platos y sus precios en el archivo "Platos.txt"
+    for (int i = 0; i < Numero_Platos; i++)
+    {
+        // usamos fprintf para escribir en el archivo.
+        // El formato "%s;%0.2f;" indica que vamos a escribir una cadena (nombre del plato) seguida de ';' para separar los campos, y luego un número decimal (precio del plato) con dos decimales, también seguido de ';'.
+        fprintf(archivo, "%s;%0.2f;", Platos[i].Nombre, Platos[i].Precio);
+    }
+
+    fclose(archivo);
+}
